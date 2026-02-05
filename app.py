47ec1caa3b9a6f4320708cd7ff14cbfb4e9c7ced
poorvi <4mh23cs107@gmail.com>
2026-02-05 20:00:52 +0530

    flask

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def abc():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
 
