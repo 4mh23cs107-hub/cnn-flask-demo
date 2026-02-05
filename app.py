7fa6f50cb7588f36563302698a49adee8be063d6
poorvi <4mh23cs107@gmail.com>
2026-02-05 19:52:19 +0530

    flask

---
+++ app.py
@@ -0,0 +1,18 @@
+import os 
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/<name>')
+def name(name):
+    return render_template('name.html', name=name,image=name+".png")
+@app.route('/abc')
+def abc():
+    return render_template('abc.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
